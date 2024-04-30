48649a247fba8cc9753d6047eefdcc6d85fe719c
aminata30 <aminata.tamanate@done.fr>
2024-04-30 11:39:32 +0200

    creating angular project

---
+++ snapfaces/src/app/asnap/asnap.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-asnap',
+  templateUrl: './asnap.component.html',
+  styleUrl: './asnap.component.scss'
+})
+export class AsnapComponent {
+
+}
