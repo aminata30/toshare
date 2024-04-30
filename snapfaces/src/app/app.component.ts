8ff16dd522796399f38414ed529ac152478a2169
aminata30 <aminata.tamanate@done.fr>
2024-04-30 16:08:08 +0200

    creating custom properties

--- snapfaces/src/app/app.component.ts
+++ snapfaces/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Asnap } from './models/asnap.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-
+export class AppComponent implements OnInit{
+  mySnap!: Asnap;
+  myOtherSnap!: Asnap;
+  myLastSnap!: Asnap;
+  ngOnInit(){
+    this.mySnap = new Asnap(
+      "Athéna",
+      "Athena is a Greek goddess and the daughter of Zeus",
+      "/assets/Athena-Wisdom.jpg",
+      new Date(),
+      0
+    );
+    this.myOtherSnap = new Asnap(
+      "Aphrodite",
+      "Vénus is a Greek goddess and the daughter of Ouranos",
+      "/assets/goddess-aphrodite.jpg",
+      new Date(),
+      0
+    );
+    this.myLastSnap = new Asnap(
+      "Artémis",
+      "Artémis is a Greek goddess and the daughter of Zeus",
+      "/assets/artemis-goddess.jpg",
+      new Date(),
+      0
+    );
+  }
 }
+
