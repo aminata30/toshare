a1bdffaeb75d57ffad6b16f049e3f634af7d1016
aminata30 <aminata.tamanate@done.fr>
2024-04-30 13:33:37 +0200

    adding first component

--- snapfaces/src/app/asnap/asnap.component.ts
+++ snapfaces/src/app/asnap/asnap.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-asnap',
   templateUrl: './asnap.component.html',
   styleUrl: './asnap.component.scss'
 })
-export class AsnapComponent {
+export class AsnapComponent implements OnInit{
+  title!: string;
+  description!: string;
+  createDate!: Date;
+  snaps!: number;
+  imageUrl!: string;
 
+  ngOnInit() {
+    this.title = "Athéna";
+    this.description = "Athena is a Grec goddess and the daughter of Zeus"
+    this.createDate = new Date()
+    this.snaps = 6;
+    this.imageUrl = "/assets/Athena-Wisdom.jpg";
+  }
 }
