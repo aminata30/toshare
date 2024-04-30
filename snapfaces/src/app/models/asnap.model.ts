dd534f8a3856f0a9795afe6751f88bf2ace37d27
aminata30 <aminata.tamanate@done.fr>
2024-04-30 15:09:26 +0200

    create folder

---
+++ snapfaces/src/app/models/asnap.model.ts
@@ -0,0 +1,10 @@
+export class Asnap {
+    constructor(
+        public title: string, 
+        public description:string, 
+        public imageUrl: string,
+        public createdDate :Date, 
+        public snaps: number) {
+           
+    }
+}
