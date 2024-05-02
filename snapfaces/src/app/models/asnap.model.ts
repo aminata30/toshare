08354db6e42f0c275dc2ecf85e905c9443d850fd
aminata30 <aminata.tamanate@done.fr>
2024-05-02 09:44:27 +0200

    shortening lignes

--- snapfaces/src/app/models/asnap.model.ts
+++ snapfaces/src/app/models/asnap.model.ts
@@ -1,10 +1,8 @@
 export class Asnap {
-    constructor(
-        public title: string, 
-        public description:string, 
-        public imageUrl: string,
-        public createdDate :Date, 
-        public snaps: number) {
-           
-    }
+         title!: string; 
+         description!:string; 
+         imageUrl!: string;
+         createdDate !:Date; 
+         snaps!: number;
+         location?: string;
 }
