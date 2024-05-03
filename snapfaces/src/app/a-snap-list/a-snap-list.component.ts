376cafd4e277fb9a991aadc50e67df58b743e085
aminata30 <aminata.tamanate@done.fr>
2024-05-03 16:49:15 +0200

    new component listing asnaps

---
+++ snapfaces/src/app/a-snap-list/a-snap-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Asnap } from '../models/asnap.model';
+import { ASnapService } from '../services/a-snap.service';
+
+@Component({
+  selector: 'app-a-snap-list',
+  templateUrl: './a-snap-list.component.html',
+  styleUrl: './a-snap-list.component.scss'
+})
+export class ASnapListComponent implements OnInit {
+
+  aSnapsArray!: Asnap[];
+
+  constructor(private aSnapService: ASnapService){}
+
+  ngOnInit(): void{
+    this.aSnapsArray=  this.aSnapService.aSnapsArray;
+  }
+}
