b0008aea64786137f4bbeefe90300130832cdbb0
aminata30 <aminata.tamanate@done.fr>
2024-05-03 16:53:14 +0200

    introducing services

--- snapfaces/src/app/app.component.ts
+++ snapfaces/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Asnap } from './models/asnap.model';
+
 
 @Component({
   selector: 'app-root',
@@ -7,35 +7,8 @@ import { Asnap } from './models/asnap.model';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit{
-  aSnapsArray!: Asnap[];
-
-
   ngOnInit(){
-    this.aSnapsArray=[
-      {
-        title: "Athéna",
-        location: 'Agen',
-        description: "Athena is a Greek goddess and the daughter of Zeus",
-        imageUrl: "/assets/Athena-Wisdom.jpg",
-        createdDate: new Date(),
-        snaps: 150,
-      },
-      {
-        title: "Aphrodite",
-        location: 'Angers',
-        description: "Aphrodite is a Greek goddess and the daughter of Ouranos",
-        imageUrl: "/assets/goddess-aphrodite.jpg",
-        createdDate: new Date(),
-        snaps: 95,
-      },
-      {
-        title: "Artémis",
-        description: "Artémis is a Greek goddess and the daughter of Zeus",
-        imageUrl: "/assets/artemis-goddess.jpg",
-        createdDate: new Date(),
-        snaps: 149,
-      },
-    ]  
+    
   }
 }
 
